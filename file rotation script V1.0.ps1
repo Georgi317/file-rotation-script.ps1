557a238613b25b4c59f1e2869e15b403d971f9d0
Georgi317 <81401840+Georgi317@users.noreply.github.com>
2025-12-03 09:42:02 +0300

    Update file rotation script V1.0.ps1

--- file rotation script V1.0.ps1
+++ file rotation script V1.0.ps1
@@ -2,7 +2,7 @@ Param (
     #период в днях старше которого файл считается пригодным к удалению
     [int]$Period = 14 , 
     #каталог для просмотра  
-    [String]$PATH = "\\10.0.17.42\Trassir-BackUp\" ,    
+    [String]$PATH = "\\Адрес сетевой папки\Папка где лежат бэкапы\" ,    
     #включать ли вложенные каталоги
     [bool]$recurse = $true
 )
@@ -21,3 +21,4 @@ if ($recurse)
 { dir -path $PATH -recurse  | Get-OldFiles -Period $Period | Remove-Item -recurse -force }
 else
 { dir -path $PATH | Get-OldFiles -Period $Period | Remove-Item -force }
+
